d21923a9b3a4e540a4abcde175b110ac2604ba12
Antonio <vallettaantonio82@gmail.com>
2024-03-30 16:09:35 +0100

    Dichiarazione proprietà del mio oggetto

--- esercizio.js
+++ esercizio.js
@@ -1,13 +1,15 @@
 const teamMember = [
-   name
-   surname
-   age
-   city
-   hobby
-   favoriteFood
-   favoriteVideoGame
-   favoriteFilm
-   favoriteBook
-   petName
-    
+  {
+    name: "Antonio",
+    surname: "Valletta",
+    age: 30,
+    city: "Irsina",
+    hobby: "Football",
+    favoriteFood: "Sushi",
+    favoriteVideoGame: "God of War",
+    favoriteFilm: "Shutter Island",
+    favoriteBook: "",
+    petName: "",
+  }
 ]
+
