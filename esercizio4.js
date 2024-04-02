56b22b212daf49a1d0000b5ccaa8e5fea9bc8fdc
Kick33 <165158874+Kick33@users.noreply.github.com>
2024-04-02 15:19:39 +0200

    Update esercizio4.js

--- esercizio4.js
+++ esercizio4.js
@@ -1,15 +1,2 @@
-const teamMember = [
-  {
-    name: "Antonio",
-    surname: "Valletta",
-    age: 30,
-    city: "Irsina",
-    hobby: "Football",
-    favoriteFood: "Sushi",
-    favoriteVideoGame: "God of War",
-    favoriteFilm: "Shutter Island",
-    favoriteBook: "",
-    petName: "",
-  }
-]
+ 
 
