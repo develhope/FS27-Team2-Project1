f37a26c2d1d73bb64f09dab2976ed1748b5b6c80
Kick33 <165158874+Kick33@users.noreply.github.com>
2024-04-02 16:42:53 +0200

    Add files via upload

--- esercizio.js
+++ esercizio.js
@@ -1,13 +1,17 @@
-const teamMemberOne = [
-  
-    name: 
-    surname: 
-    age: 
-    city: 
-    hobby: 
-    favoriteFood: 
-    favoriteVideoGame: 
-    favoriteFilm: 
-    favoriteBook: 
-    petName: 
-    ],
+const teamMember = [
+  {
+    name: "Eros",
+    surname: "Greco",
+    age: 22,
+    city: "Verona",
+    hobby: "Botanica",
+    favoriteFood: "Patate a fette",
+    favoriteVideoGame: "Kindom Hearts",
+    favoriteFilm: "Godzilla King of the Moster",
+    favoriteBook: "Solo Leveling",
+    petName: "Pongo",
+  },
+];
+
+
+ 
