a22499d7f3801561ce95e81359f8a977c1af890e
Kick33 <165158874+Kick33@users.noreply.github.com>
2024-04-02 15:19:19 +0200

    Update esercizio5.js

--- esercizio5.js
+++ esercizio5.js
@@ -1,14 +1,1 @@
-const teamMember = [
-   {
-      name : 'Silas',
-      surname : 'Bonsu',
-      age : 25,
-      city : 'Correggio',
-      hobby : 'Atletica',
-      favoriteFood : 'Pizza',
-      favoriteVideoGame : 'Horizon zero down',
-      favoriteFilm : 'Star wars',
-      favoriteBook : 'Il signore degli anelli',
-      petName : 'Bethoven',
-   }
-]   
+ 
