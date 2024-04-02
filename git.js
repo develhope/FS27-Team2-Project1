b3335b0372fc694bd50da58d28235459129bdff7
Kick33 <165158874+Kick33@users.noreply.github.com>
2024-04-02 15:15:51 +0200

    Update git.js

--- git.js
+++ git.js
@@ -1,14 +1,1 @@
-const teamMember = [
-    {
-      name: "Eros",
-      surname: "Greco",
-      age: 22,
-      city: "Verona",
-      hobby: "Botanica",
-      favoriteFood: "Patate a fette",
-      favoriteVideoGame: "Kindom Hearts",
-      favoriteFilm: "Godzilla King of the Moster",
-      favoriteBook: "Solo Leveling",
-      petName: "Pongo",
-    },
-  ];
+
